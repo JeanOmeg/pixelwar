6d3c5afeefceca1d8c1dad2e652a82f05bdbdadb
BernardoAugus <bernardo.11salgado@hotmail.com>
2024-07-28 20:06:48 -0300

    Criar tela de seleção de mapas

--- src/levels/start-screen.ts
+++ src/levels/start-screen.ts
@@ -5,11 +5,16 @@ import { Resources } from '../resources'
 import { SCALE } from '../config'
 
 export class StartScreen extends ex.Scene {
+
   title!: ex.Actor
   p1VsCpuButton!: ex.Actor
   p1VsP2Button!: ex.Actor
   cpuVscpuButton!: ex.Actor
-  nextLevel!: string
+
+  map1Button!: ex.Actor
+  map2Button!: ex.Actor
+  ramdomMapButton!: ex.Actor
+  buttonFont!: ex.Font
 
   override onInitialize(engine: ex.Engine): void {
     const mobile = this.isMobile()
@@ -17,7 +22,6 @@ export class StartScreen extends ex.Scene {
       this.setLandscapeAndFullscreen()
     }
     this.engine = engine
-    this.nextLevel = this.selectMap()
 
     const titleFont = new ex.Font({
       family: 'notjamslab14',
@@ -47,7 +51,7 @@ export class StartScreen extends ex.Scene {
 
     this.add(this.title)
 
-    const buttonFont = new ex.Font({
+    this.buttonFont = new ex.Font({
       family: 'notjamslab14',
       size: 32 * SCALE.x,
       unit: ex.FontUnit.Px,
@@ -66,7 +70,7 @@ export class StartScreen extends ex.Scene {
 
     const p1VsCpuText = new ex.Text({
       text: 'P1 vs CPU',
-      font: buttonFont,
+      font: this.buttonFont,
     })
 
     this.p1VsCpuButton.graphics.use(new ex.GraphicsGroup({
@@ -93,7 +97,7 @@ export class StartScreen extends ex.Scene {
       Resources.SelectSound.play()
       localStorage.setItem('start_screen', 'p1vscpu')
       await this.p1VsCpuButton.actions.blink(100, 100, 6).toPromise()
-      this.engine.goToScene(this.nextLevel)
+      this.selectMap()
     })
 
     this.p1VsCpuButton.scale = SCALE
@@ -110,7 +114,7 @@ export class StartScreen extends ex.Scene {
 
     const p1VsP2Text = new ex.Text({
       text: 'P1 vs P2',
-      font: buttonFont,
+      font: this.buttonFont,
     })
 
     this.p1VsP2Button.graphics.use(new ex.GraphicsGroup({
@@ -138,7 +142,7 @@ export class StartScreen extends ex.Scene {
       Resources.SelectSound.play()
       localStorage.setItem('start_screen', 'p1vsp2')
       await this.p1VsP2Button.actions.blink(100, 100, 6).toPromise()
-      this.engine.goToScene(this.nextLevel)
+      this.selectMap()
     })
 
     this.p1VsP2Button.scale = SCALE
@@ -155,7 +159,7 @@ export class StartScreen extends ex.Scene {
 
     const cpuVscputext = new ex.Text({
       text: 'CPU vs CPU',
-      font: buttonFont,
+      font: this.buttonFont,
     })
 
     this.cpuVscpuButton.graphics.use(new ex.GraphicsGroup({
@@ -183,7 +187,7 @@ export class StartScreen extends ex.Scene {
       Resources.SelectSound.play()
       localStorage.setItem('start_screen', 'cpuvscpu')
       await this.cpuVscpuButton.actions.blink(100, 100, 6).toPromise()
-      this.engine.goToScene(this.nextLevel)
+      this.selectMap()
     })
 
     this.cpuVscpuButton.scale = SCALE
@@ -192,7 +196,6 @@ export class StartScreen extends ex.Scene {
 
   _subscriptions: ex.Subscription[] = []
   onActivate(): void {
-    this.nextLevel = this.selectMap()
     if (this.p1VsCpuButton.isKilled()) {
       this.add(this.p1VsCpuButton)
     }
@@ -212,9 +215,174 @@ export class StartScreen extends ex.Scene {
     this._subscriptions.forEach(h => h.close())
   }
 
-  selectMap(): string {
-    const maps = ['level1-map1', 'level1-map2']
-    return maps[Math.floor(Math.random() * 2)]
+  selectMap() {
+    this.map1Button = new ex.Actor({
+      name: 'map1',
+      pos: ex.vec(650, 400),
+      width: 210 * SCALE.x,
+      height: 45 * SCALE.y,
+      color: ex.Color.Red,
+      coordPlane: ex.CoordPlane.Screen
+    })
+
+    const choseMap1text = new ex.Text({
+      text: 'Map 1',
+      font: this.buttonFont,
+    })
+
+    const loader = new ex.Loader();
+    const mapTexture1 = Resources.Map1
+    const mapTexture2 = Resources.Map2
+    
+    loader.addResource(mapTexture1);
+    loader.addResource(mapTexture2);
+
+    const mapSprite1 = mapTexture1.toSprite();
+    mapSprite1.scale = ex.vec(0.05, 0.05);
+    const mapSprite2 = mapTexture2.toSprite();
+    mapSprite2.scale = ex.vec(0.05, 0.05);
+
+    this.map1Button.graphics.use(new ex.GraphicsGroup({
+      members: [
+        {
+          graphic: new ex.Rectangle({
+            width: this.map1Button.width,
+            height: this.map1Button.height,
+            color: this.map1Button.color,
+            lineWidth: 4,
+          }),
+          offset: ex.vec(0, 0),
+        },
+        {
+          graphic: mapSprite1,
+          offset: ex.vec(5,
+            this.map1Button.height / 2 - mapSprite1.height / 2),
+        },
+        {
+          graphic: choseMap1text,
+          offset: ex.vec(mapSprite1.width + 10,
+            this.map1Button.height / 2 - choseMap1text.height / 2),
+        }
+      ]
+    }))
+
+    this.map1Button.on('pointerup', async () => {
+      await this.map1Button.actions.blink(100, 100, 6).toPromise()
+      
+      this.engine.goToScene('level1-map1')
+      this.killDiffMap(this.map1Button.name)
+    })
+
+    this.map1Button.scale = SCALE
+    this.add(this.map1Button)
+
+    this.map2Button = new ex.Actor({
+      name: 'map2',
+      pos: ex.vec(650, 520),
+      width: 210 * SCALE.x,
+      height: 45 * SCALE.y,
+      color: ex.Color.Blue,
+      coordPlane: ex.CoordPlane.Screen
+    })
+
+    const choseMap2text = new ex.Text({
+      text: 'Map 2',
+      font: this.buttonFont,
+    })
+
+    this.map2Button.graphics.use(new ex.GraphicsGroup({
+      members: [
+        {
+          graphic: new ex.Rectangle({
+            width: this.map2Button.width,
+            height: this.map2Button.height,
+            color: this.map2Button.color,
+            lineWidth: 4,
+          }),
+          offset: ex.vec(0, 0),
+        },
+        {
+          graphic: mapSprite2,
+          offset: ex.vec(5,
+            this.map2Button.height / 2 - mapSprite2.height / 2),
+        },
+        {
+          graphic: choseMap2text,
+          offset: ex.vec(mapSprite2.width + 10,
+            this.map2Button.height / 2 - choseMap2text.height / 2),
+        }
+      ]
+    }))
+
+    this.map2Button.on('pointerup', async () => {
+      await this.map2Button.actions.blink(100, 100, 6).toPromise()
+      
+      this.engine.goToScene('level1-map2')
+      this.killDiffMap(this.map2Button.name)
+    })
+
+    this.map2Button.scale = SCALE
+    this.add(this.map2Button)
+
+    this.ramdomMapButton = new ex.Actor({
+      name: 'ramdomMap',
+      pos: ex.vec(650, 640),
+      width: 210 * SCALE.x,
+      height: 45 * SCALE.y,
+      color: ex.Color.Red,
+      coordPlane: ex.CoordPlane.Screen
+    })
+
+    const choseRamdomMaptext = new ex.Text({
+      text: 'Random Map',
+      font: this.buttonFont,
+    })
+
+    this.ramdomMapButton.graphics.use(new ex.GraphicsGroup({
+      members: [
+        {
+          graphic: new ex.Rectangle({
+            width: this.ramdomMapButton.width,
+            height: this.ramdomMapButton.height,
+            color: this.ramdomMapButton.color,
+            lineWidth: 4,
+          }),
+          offset: ex.vec(0, 0),
+        },
+        {
+          graphic: choseRamdomMaptext,
+          offset: ex.vec(this.ramdomMapButton.width / 2 - choseRamdomMaptext.width / 2,
+            this.ramdomMapButton.height / 2 - choseRamdomMaptext.height / 2),
+        }
+      ]
+    }))
+
+    this.ramdomMapButton.on('pointerup', async () => {
+      await this.ramdomMapButton.actions.blink(100, 100, 6).toPromise()
+      
+      const maps = ['level1-map1', 'level1-map2']
+      this.engine.goToScene(maps[Math.floor(Math.random() * 2)])
+      this.killDiffMap(this.ramdomMapButton.name)
+    })
+
+    this.ramdomMapButton.scale = SCALE
+    this.add(this.ramdomMapButton)
+    
+    this.p1VsCpuButton.kill()
+    this.p1VsP2Button.kill()
+    this.cpuVscpuButton.kill()
+  }
+
+  killDiffMap(nameButton: string) {
+    if (nameButton !== this.map1Button.name) {
+      this.map1Button.kill()
+    }
+    if (nameButton !== this.map2Button.name) {
+      this.map2Button.kill()
+    }
+    if (nameButton !== this.ramdomMapButton.name) {
+      this.ramdomMapButton.kill()
+    }
   }
 
   isMobile() {
