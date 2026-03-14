e0d697abd7aaa98ffe8e57b77153572cf5130c81
jeanfernandespro <jean.fernandes@prodigitalhr.com.br>
2026-03-14 10:50:13 -0300

    - Build deploy

---
+++ build.js
@@ -0,0 +1,14 @@
+const { execSync } = require('child_process')
+
+function run(cmd) {
+  execSync(cmd, { stdio: 'inherit' })
+}
+
+console.log('✅ Criando build')
+run('parcel build ./index.html --dist-dir ./docs --public-url ./')
+
+console.log('✅ Adicionando os arquivos')
+run('git add .')
+
+console.log('✅ Commit')
+run('git commit -m "- Build deploy"')
