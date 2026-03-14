d6350935fad995cead32ce2c0ce8c17f3382ae85
jeanfernandespro <jean.fernandes@prodigitalhr.com.br>
2026-03-14 11:07:35 -0300

    - Build deploy

--- build.js
+++ build.js
@@ -4,20 +4,20 @@ function run(cmd) {
   execSync(cmd, { stdio: 'inherit' })
 }
 
-console.log('✅ Limpando arquivos')
+console.log('🗑️ Limpando arquivos')
 run('rm -rf ./parcel-cache')
 run('rm -rf ./docs')
 run('rm -rf ./dist')
 
-console.log('✅ Criando build')
+console.log('🤖 Criando build')
 run('parcel build ./index.html --dist-dir ./docs --public-url ./')
 
-console.log('✅ Adicionando os arquivos')
+console.log('➕ Adicionando os arquivos')
 run('git add .')
 
 try {
   console.log('✅ Commit')
   run('git commit -m "- Build deploy"')
 } catch (error) {
-  console.log('ℹ️ Nada para commitar ou commit falhou')
+  console.log('❌ Nada para commitar ou commit falhou')
 }
